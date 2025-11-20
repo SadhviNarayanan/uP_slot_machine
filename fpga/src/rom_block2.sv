152193f2a4aacc390b993c1794c625a8140baa1c
SadhviNarayanan <146152067+SadhviNarayanan@users.noreply.github.com>
2025-11-19 17:17:54 -0800

    Add UNIQUE_ID parameter to rom_block2 module

--- fpga/src/rom_block2.sv
+++ fpga/src/rom_block2.sv
@@ -1,28 +1,26 @@
-module rom_block2  #(
-    // HSOSC divider: "0b00"=48, "0b01"=24, "0b10"=12, "0b11"=6 MHz
-    parameter string text_file
+module rom_block2 #(
+    parameter string text_file,
+    parameter int UNIQUE_ID = 0  // Forces separate synthesis for each instance
 ) (
     input  logic clk,
     input  logic [7:0] address,
-	output logic [15:0] rgb
+    output logic [15:0] rgb
 );
-	localparam SPRITE_WIDTH = 64;
+    localparam SPRITE_WIDTH = 64;
     localparam DATA_WIDTH = 16;
-    localparam TOTAL_WORDS = 256; // 7168 words total
-    localparam ADDRESS_WIDTH = 8;        // 13 bits for 7168 words
+    localparam TOTAL_WORDS = 256;
+    localparam ADDRESS_WIDTH = 8;
     
-	(* ram_style = "block" *) 
-    logic [DATA_WIDTH-1:0] single_bram1 [0:TOTAL_WORDS-1]; 
-
-
-    // NOTE: need a file with "all_sprites_combined.mem" (7168 lines) --> should have all the sprites in 1 hopefully it uses 28 bram blcocks
+    (* syn_ramstyle = "EBR" *)
+    (* syn_preserve = 1 *)
+    logic [DATA_WIDTH-1:0] single_bram1 [0:TOTAL_WORDS-1];
+    
     initial begin 
-		$readmemh(text_file, single_bram1);
+        $readmemh(text_file, single_bram1);
     end
-
-	always_ff @(posedge clk) begin 
-		rgb      <= single_bram1[address]; 
-	end
-
-
+    
+    always_ff @(posedge clk) begin 
+        rgb <= single_bram1[address]; 
+    end
 endmodule
+
