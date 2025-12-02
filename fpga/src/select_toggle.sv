45a46b46fa4f855481a33976fbf13b02d997fd02
SadhviNarayanan <146152067+SadhviNarayanan@users.noreply.github.com>
2025-12-01 18:58:28 -0800

    updated select toggle

--- fpga/src/select_toggle.sv
+++ fpga/src/select_toggle.sv
@@ -9,22 +9,26 @@ module select_toggle(
 
   logic [23:0] counter;
   logic [2:0] freq_counter;
+
+  // For 25MHz clock and 5 displays at 60Hz each:
+  // 25,000,000 / (60 * 5) = 83,333 cycles per display
+  localparam CYCLES_PER_DISPLAY = 83333;
   
   // clock is toggling at 60Hz
   always_ff @(posedge clk, negedge reset_n) begin
     if(reset_n == 0) begin
-        counter <= 0;
-        freq_counter <= 0;
-    end else if (counter == 2000000) begin
-        counter <= 0;
+      counter <= 0;
+      freq_counter <= 0;
+    end else if (counter == CYCLES_PER_DISPLAY - 1) begin
+      counter <= 0;
 
-		if (freq_counter == 3'd4) begin
-			freq_counter <= 0;
-		end else begin
-			freq_counter <= freq_counter + 1;  // increments the ouput to create 60Hz frequency
-		end
+      if (freq_counter == 3'd4) begin
+        freq_counter <= 0;
+      end else begin
+        freq_counter <= freq_counter + 1;  // increments the ouput to create 60Hz frequency
+      end
    end else begin
-       counter <= counter + 1;
+      counter <= counter + 1;
    end
   end
     
